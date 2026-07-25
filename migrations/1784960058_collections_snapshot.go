4438a9dcfe201c9bad8a694e558e43c2ce3b4729
asano69 <186772504+asano69@users.noreply.github.com>
2026-07-25 15:14:28 +0900

    change schema

--- migrations/1784960058_collections_snapshot.go
+++ migrations/1784960058_collections_snapshot.go
@@ -943,19 +943,18 @@ func init() {
 						"type": "text"
 					},
 					{
+						"cascadeDelete": false,
+						"collectionId": "pbc_3607937828",
 						"help": "",
 						"hidden": false,
-						"id": "file3309110367",
+						"id": "relation3309110367",
 						"maxSelect": 0,
-						"maxSize": 0,
-						"mimeTypes": null,
+						"minSelect": 0,
 						"name": "image",
 						"presentable": false,
-						"protected": false,
 						"required": false,
 						"system": false,
-						"thumbs": null,
-						"type": "file"
+						"type": "relation"
 					},
 					{
 						"autogeneratePattern": "",
@@ -1878,6 +1877,99 @@ func init() {
 				"indexes": [],
 				"listRule": null,
 				"name": "settings",
+				"system": false,
+				"type": "base",
+				"updateRule": null,
+				"viewRule": null
+			},
+			{
+				"createRule": null,
+				"deleteRule": null,
+				"fields": [
+					{
+						"autogeneratePattern": "[a-z0-9]{15}",
+						"help": "",
+						"hidden": false,
+						"id": "text3208210256",
+						"max": 15,
+						"min": 15,
+						"name": "id",
+						"pattern": "^[a-z0-9]+$",
+						"presentable": false,
+						"primaryKey": true,
+						"required": true,
+						"system": true,
+						"type": "text"
+					},
+					{
+						"autogeneratePattern": "",
+						"help": "",
+						"hidden": false,
+						"id": "text3518522040",
+						"max": 0,
+						"min": 0,
+						"name": "hash",
+						"pattern": "",
+						"presentable": false,
+						"primaryKey": false,
+						"required": true,
+						"system": false,
+						"type": "text"
+					},
+					{
+						"help": "",
+						"hidden": false,
+						"id": "select2063623452",
+						"maxSelect": 0,
+						"name": "status",
+						"presentable": false,
+						"required": false,
+						"system": false,
+						"type": "select",
+						"values": [
+							"imported"
+						]
+					},
+					{
+						"help": "",
+						"hidden": false,
+						"id": "file3309110367",
+						"maxSelect": 0,
+						"maxSize": 0,
+						"mimeTypes": null,
+						"name": "image",
+						"presentable": false,
+						"protected": false,
+						"required": false,
+						"system": false,
+						"thumbs": null,
+						"type": "file"
+					},
+					{
+						"hidden": false,
+						"id": "autodate2990389176",
+						"name": "created",
+						"onCreate": true,
+						"onUpdate": false,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					},
+					{
+						"hidden": false,
+						"id": "autodate3332085495",
+						"name": "updated",
+						"onCreate": true,
+						"onUpdate": true,
+						"presentable": false,
+						"system": false,
+						"type": "autodate"
+					}
+				],
+				"id": "pbc_3607937828",
+				"indexes": [],
+				"listRule": null,
+				"name": "images",
 				"system": false,
 				"type": "base",
 				"updateRule": null,
